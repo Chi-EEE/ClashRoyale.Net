b2710fb0dc8e4ed8a0df85a7001ca016e128e47e
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-12-22 20:51:14 +0000

    Added 4 projects and added CSV parser for CSV files included in game

---
+++ ClashRoyale/ClashRoyale.AI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClashRoyale.AI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ClashRoyale/ClashRoyale/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClashRoyale.Files;
+
+namespace ClashRoyale
+{
+    internal class Program
+    {
+        public static Csv Csv { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Csv = new Csv();
+        }
+        public static void Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
